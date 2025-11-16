80403764d78888b09c9eb4009a01e469d9648d90
Segni Mideksa <segnim21@gmail.com>
2025-11-17 01:42:38 +0300

    update file

---
+++ 1-multi_languages.js
@@ -0,0 +1,6 @@
+//let arrayName ='C is fun', 'python is cool', 'javascript is amazing';
+//for(i=0;i<arrayName.length();i++){
+    //console.log(arrayName.length[i]);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
